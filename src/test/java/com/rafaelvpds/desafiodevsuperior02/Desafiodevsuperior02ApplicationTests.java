7b5c29780a101085322d849eb09d07242aa1f602
Rafael <rafaelvpds.dev09@gmail.com>
2023-12-29 14:04:27 -0300

    first commit

---
+++ src/test/java/com/rafaelvpds/desafiodevsuperior02/Desafiodevsuperior02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelvpds.desafiodevsuperior02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafiodevsuperior02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
